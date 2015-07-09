8b8d96b80a2660d6685061a5202c0c93840386d4
Clement Chan <clement_chan_pig@Clements-MacBook-Pro.local>
2015-07-09 00:26:26 -0400

    Added Initial Design of Dating App

---
+++ Dating_App/ViewControllerRegister.swift
@@ -0,0 +1,28 @@
+//
+//  ViewControllerLogin.swift
+//  Dating_App
+//
+//  Created by Clement Chan on 7/8/15.
+//  Copyright (c) 2015 Clement Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerRegister: UIViewController{
+    
+    @IBOutlet weak var FirstName: UITextField!
+    @IBOutlet var LastName: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
