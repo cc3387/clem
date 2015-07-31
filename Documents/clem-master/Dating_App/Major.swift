c40a60aa6f9128c91e616d5a6c1216f3eec5344d
Clement Chan <clement_chan_pig@Clements-MacBook-Pro.local>
2015-07-30 20:22:41 -0400

    Added new dating app

---
+++ Documents/clem-master/Dating_App/Major.swift
@@ -0,0 +1,21 @@
+//
+//  Major.swift
+//  Simple
+//
+//  Created by Clement Chan on 7/27/15.
+//  Copyright (c) 2015 Clement Chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Major: UIViewController{
+    
+    override func viewDidLoad() {
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        // Dispose of any resources that can be recreated.
+    }
+}
