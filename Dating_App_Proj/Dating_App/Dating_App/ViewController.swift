331bf7bc72f7322a824b23bb39bd90544f88a55d
Clement Chan <clement_chan_pig@Clements-MacBook-Pro.local>
2015-09-19 12:02:53 -0400

    New dating app information included

---
+++ Dating_App_Proj/Dating_App/Dating_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dating_App
+//
+//  Created by Clement Chan on 7/8/15.
+//  Copyright (c) 2015 Clement Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
